1109a33f3979aaf05d5c2f627c3b9456d556ac51
Arthur Carvalhais <art_moser@hotmail.com>
2024-08-20 23:53:22 -0300

    Starting component development

---
+++ tcg-salesforce/force-app/main/default/game-module/lwc/deckManagement/deckManagement.js
@@ -0,0 +1,5 @@
+import { LightningElement, track, api } from 'lwc';
+
+export default class DeckManagement extends LightningElement {
+    @api playerLoginCode;
+}
--- tcg-salesforce/force-app/main/default/game-module/lwc/game/game.js
+++ tcg-salesforce/force-app/main/default/game-module/lwc/game/game.js
@@ -1,3 +1,6 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
 
-export default class Game extends LightningElement {}
+export default class Game extends LightningElement {
+    @track playerLoginCode = '123';
+    
+}
