47d9ca1eedc8d09d143e1f427db2297eac433e61
Arthur Moser Carvalhais <art_moser@hotmail.com>
2024-08-22 14:58:49 -0300

    Adding deck's cards update

--- tcg-salesforce/force-app/main/default/game-module/deck-management/lwc/deckManagement/deckManagement.js
+++ tcg-salesforce/force-app/main/default/game-module/deck-management/lwc/deckManagement/deckManagement.js
@@ -2,6 +2,7 @@ import { LightningElement, track, api } from 'lwc';
 
 import getPlayerCards from '@salesforce/apex/DeckManagementController.getPlayerCards';
 import getPlayerDecks from '@salesforce/apex/DeckManagementController.getPlayerDecks';
+import updateDeckCards from '@salesforce/apex/DeckManagementController.updateDeckCards';
 
 export default class DeckManagement extends LightningElement {
     @api playerLoginCode;
@@ -9,28 +10,43 @@ export default class DeckManagement extends LightningElement {
     @track playerDecks = [];
     @track removedDeckCards = [];
     @track currentDeck = {};
-    
 
-    async connectedCallback() {
+    connectedCallback() {
+        this.initDeckManagement();
+    }
+
+    async initDeckManagement() {
         try {
             this.playerCards = await getPlayerCards({playerLoginCode: this.playerLoginCode});
             this.playerDecks = await getPlayerDecks({playerLoginCode: this.playerLoginCode});
-            this.setCurrentDeck();
+            this.setCurrentDeck(null);
         } catch (error) {
-            console.error('Error fetching greeting:', error);
+            console.error('Error fetching data:', error);
         }
     }
 
-    setCurrentDeck() {
+    setCurrentDeck(deckSelectedId) {
         if(this.playerDecks.length == 0) { return; }
 
         let deckId = '';
         let localPlayerDecks = [...this.playerDecks];
 
+        localPlayerDecks = this.unselectAllDecks(localPlayerDecks);
+
         for(let deck of localPlayerDecks) {
-            deck.isSelected = true;
-            deckId = deck.Id;
-            break;
+            if(!deckSelectedId) {
+                deck.isSelected = true;
+                deckId = deck.Id;
+                this.currentDeck = deck;
+                break;
+            }
+
+            if(deck.Id == deckSelectedId) {
+                this.currentDeck = deck;
+                deck.isSelected = true;
+                deckId = deckSelectedId;
+                break;
+            }
         }
 
         this.playerDecks = localPlayerDecks;
@@ -46,11 +62,23 @@ export default class DeckManagement extends LightningElement {
         for(let playerCard of localPlayerCards) {
             let deckCard = deckCardsById[playerCard.Id];
             if(deckCard) {
+                playerCard.initialState = 'selected';
                 playerCard.isSelected = true;
+            } else {
+                playerCard.initialState = 'unselected';
             }
         }
 
         this.playerCards = localPlayerCards;
+    }
+
+    unselectAllDecks(playerDecks) {
+        for(let playerDeck of playerDecks) {
+            playerDeck.isDeleted = false;
+            playerDeck.isSelected = false;
+        }
+
+        return playerDecks;
     }
 
     unselectAllCards(playerCards) {
@@ -88,7 +116,7 @@ export default class DeckManagement extends LightningElement {
 
                 if(!playerCard.isSelected) {
                     playerCard.isDeleted = true;
-                }
+            }
 
                 if(playerCard.isSelected) {
                     playerCard.isDeleted = false;
@@ -99,5 +127,62 @@ export default class DeckManagement extends LightningElement {
 
             this.playerCards = localPlayerCards;
         }
+    }
+
+    handleSaveDeckModifications() {
+        let deckCardsToRemove = this.generateDeckUpdateObject(this.currentDeck.Id, this.getRemovedDeckCards());
+        let deckCardsToAdd = this.generateDeckUpdateObject(this.currentDeck.Id, this.getAddedDeckCards());
+
+        updateDeckCards({ 
+                        deckCardsToRemove : deckCardsToRemove, 
+                        deckCardsToAdd : deckCardsToAdd 
+                    })
+        .then(response => {
+            this.initDeckManagement();
+        })
+        .catch(error => {
+            console.log(error)
+        });
+    }
+
+    getRemovedDeckCards() {
+        let removedPlayerCardsFromDeck = [];
+
+        for(let playerCard of this.playerCards) {
+            if(playerCard.Id && playerCard.isDeleted) {
+                removedPlayerCardsFromDeck.push(playerCard);
+            }
+        }
+
+        return removedPlayerCardsFromDeck;
+    }
+
+    getAddedDeckCards() {
+        let addedPlayerCardsToDeck = [];
+
+        for(let playerCard of this.playerCards) {
+            if(playerCard.initialState == 'unselected' && playerCard.isSelected) {
+                addedPlayerCardsToDeck.push(playerCard);
+            }
+        }
+
+        return addedPlayerCardsToDeck;
+    }
+
+    generateDeckUpdateObject(deckId, playerCardsToUpdate) {
+        let deckUpdateObject = {
+            deckId: deckId,
+            playerCardIds: []
+        }
+
+        for(let playerCard of playerCardsToUpdate) {
+            deckUpdateObject.playerCardIds.push(playerCard.Id);
+        }
+
+        return deckUpdateObject;
+    }
+
+    handleCancelDeckModifications() {
+        this.initDeckManagement();
     }
 }
